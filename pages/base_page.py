87a9b77323ad19b9e7dc0db40d492a2a453ccb90
“Muzyria” <fila090580@gmail.com>
2023-04-07 17:05:21 +0300

    new

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+
 class BasePage:
     def __init__(self, driver, url):
         self.driver = driver
@@ -5,3 +8,6 @@ class BasePage:
 
     def open(self):
         self.driver.get(self.url)
+
+    def element_is_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
