377985600ce05ca7518ba9f2553ab198da106409
“Muzyria” <fila090580@gmail.com>
2023-04-07 14:26:32 +0300

    new

--- conftest.py
+++ conftest.py
@@ -6,3 +6,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture(scope="function")
 def driver():
     driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+    yield driver
+    driver.quit()
