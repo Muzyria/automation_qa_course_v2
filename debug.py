080ebc5774d21a7e8ca6eb0b7000c6c7a303b69a
“Muzyria” <fila090580@gmail.com>
2023-04-18 18:34:39 +0300

    new

---
+++ debug.py
@@ -0,0 +1,2 @@
+data_1 = ['Commands', 'WorkSpace', 'React', 'Angular', 'Veu', 'Public', 'Classified', 'Downloads', 'Word File.doc', 'Excel File.doc']
+data_2 = ['commands', 'workspace', 'react', 'angular', 'veu', 'public', 'classified', 'downloads', 'wordFile', 'excelFile']
