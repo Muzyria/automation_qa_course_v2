7d9646c3d5de2adade865925ccb54fcba5da180d
“Muzyria” <fila090580@gmail.com>
2023-04-28 17:15:48 +0300

    new

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -73,7 +73,7 @@ class TestElements:
 
             web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
             web_table_page.open()
-            for _ in range(10):
+            for _ in range(5):
                 key_word = web_table_page.add_new_person()[random.randint(0, 5)]
                 time.sleep(1)
                 web_table_page.search_some_person(key_word)
@@ -88,3 +88,31 @@ class TestElements:
                 web_table_page.search_clear(key_word)
                 # time.sleep(1)
 
+        def test_web_table_update_person_info(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            lastname = web_table_page.add_new_person()[1]
+            time.sleep(1)
+            web_table_page.search_some_person(lastname)
+            time.sleep(1)
+            age = web_table_page.update_person_info()
+            time.sleep(1)
+            row = web_table_page.check_search_person()
+            time.sleep(1)
+            assert age in row, "the person card has not been changed"
+
+        # def test_web_table_delete_person(self, driver):
+        #     web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+        #     web_table_page.open()
+        #     email = web_table_page.add_new_person()[3]
+        #     web_table_page.search_some_person(email)
+        #     web_table_page.delete_person()
+        #     text = web_table_page.check_deleted()
+        #     assert text == "No rows found"
+        #
+        # def test_web_table_change_count_row(self, driver):
+        #     web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+        #     web_table_page.open()
+        #     count = web_table_page.select_up_to_some_rows()
+        #     assert count == [5, 10, 20, 25, 50,
+        #                      100], 'The number of rows in the table has not been changed or has changed incorrectly'
