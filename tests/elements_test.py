46b55fb69fc56eb7e1c9b8deab654e403931408f
“Muzyria” <fila090580@gmail.com>
2023-08-10 17:10:48 +0300

    new

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -126,10 +126,7 @@ class TestElements:
             double = button_page.click_on_different_button('double')
             right = button_page.click_on_different_button('right')
             click = button_page.click_on_different_button('click')
-            print()
-            print(double)
-            print(right)
-            print(click)
-            # assert double == ''
-            # assert right ==
-            # assert click ==
+
+            assert double == "You have done a double click", "The double click button was not pressed"
+            assert right == "You have done a right click", "The right click button was not pressed"
+            assert click == "You have done a dynamic click", "The dynamic click button was not pressed"
