9e78f730e746e246d87ad45e741cfc9c27fe1aa0
“Muzyria” <fila090580@gmail.com>
2023-08-15 16:39:11 +0300

    new

--- data/data.py
+++ data/data.py
@@ -9,8 +9,20 @@ class Person:
     age: int = None
     salary: int = None
     department: str = None
-
     email: str = None
     current_address: str = None
     permanent_address: str = None
+    mobile: str = None
 
+
+@dataclass
+class Color:
+    color_name: list = None
+
+
+@dataclass
+class Date:
+    day: str = None
+    month: str = None
+    year: str = None
+    time: str = None
--- generator/generator.py
+++ generator/generator.py
@@ -1,11 +1,11 @@
 import random
 
-from data.data import Person
+from data.data import Person, Color, Date
 from faker import Faker
 
 faker_ru = Faker('ru_RU')
+fake_en = Faker('En')
 Faker.seed()
-
 
 def generated_person():
     yield Person(
@@ -20,3 +20,25 @@ def generated_person():
         permanent_address=faker_ru.address(),
     )
 
+
+def generated_file():
+    path = rf'E:\automation_qa_course\filetest{random.randint(0, 999)}.txt'
+    file = open(path, 'w+')
+    file.write(f'Hello World{random.randint(0, 999)}')
+    file.close()
+    return file.name, path
+
+
+def generated_color():
+    yield Color(
+        color_name=["Red", "Blue", "Green", "Yellow", "Purple", "Black", "White", "Voilet", "Indigo", "Magenta", "Aqua"]
+    )
+
+
+def generated_date():
+    yield Date(
+        year=fake_en.year(),
+        month=fake_en.month_name(),
+        day=fake_en.day_of_month(),
+        time="12:00"
+    )
